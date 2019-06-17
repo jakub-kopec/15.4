87cd12f4d65126b1ec12abb748014508e02a8a6b
jakub-kopec <agh.jakub@gmail.com>
2019-06-17 23:07:04 +0200

    15.4 ready

--- script-compiled.js
+++ script-compiled.js
@@ -1,22 +1,33 @@
-const sayHello = () => alert('Hello world!');
+'use strict';
+
+var sayHello = function sayHello() {
+    return alert('Hello world!');
+};
 sayHello();
 
 //Zadanie 1
-const hello = 'Hello';
-const world = 'World';
+var hello = 'Hello';
+var world = 'World';
 
-const hellorworld = `${hello}${world}`;
+var hellorworld = '' + hello + world;
 
 // Zadanie 2
-const multiply = (a, b = 1) => a * b;
+var multiply = function multiply(a) {
+    var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+    return a * b;
+};
 console.log(multiply(2, 5));
 console.log(multiply(6, 6));
 console.log(multiply(5));
 
 // Zadanie 3
-const avr = (...args) => {
-    let sum = 0;
-    args.forEach(arg => {
+var avr = function avr() {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+    }
+
+    var sum = 0;
+    args.forEach(function (arg) {
         sum += arg;
     });
     return sum / args.length;
@@ -27,11 +38,13 @@ console.log(average(1, 3));
 console.log(average(1, 3, 6, 6));
 
 // Zadanie 4
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-console.log(average(...grades));
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average.apply(undefined, grades));
 
 // Zadanie 5
-const data = [1, 4, 'Iwona', false, 'Nowak'];
-const [,, firstname,, lastname] = data;
+var data = [1, 4, 'Iwona', false, 'Nowak'];
+var firstname = data[2],
+    lastname = data[4];
+
 console.log(firstname);
 console.log(lastname);
